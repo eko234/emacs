ee8155cb051cda1e6485c2fe55b419dabcdaa962
Luis Alfonso Buelvas Betancourt <38670883+eko234@users.noreply.github.com>
2021-05-04 18:19:26 -0500

    Update init.el
    sadsadsad

--- init.el
+++ init.el
@@ -11,13 +11,18 @@
 	paredit
 	twilight-bright-theme
 	linum-relative
+	kakoune
+	evil-paredit
 	which-key
+	visual-regexp-steroids
 	projectile
 	ido
 	racket-mode
 	magit
 	company
+	company-fuzzy
 	direx
+	general
 	helm
 	treemacs
 	treemacs-evil
@@ -45,12 +50,88 @@
 (setq visible-bell t)
 (display-line-numbers-mode)
 (setq display-line-numbers 'relative)
+(show-paren-mode)
 
-(use-package evil-surround
-  :load-path "vendor/evil-surround"
-  :hook ((after-init . global-evil-surround-mode)))
+(use-package evil-surround			       
+  :ensure t
+  :hook ((after-init . global-evil-surround-mode))
+  :config
+  (global-evil-surround-mode 1)) 
+
+(use-package general
+  :ensure t)
+
+(use-package evil
+  :config
+  (evil-mode)
+  ;; (general-nmap
+  ;;  :prefix "spc"
+  ;;  :prefix-map 'evil-leader-prefix-map
+  ;;  )
+  (setq evil-cross-lines t)
+  (setq evil-move-cursor-back nil)
+  (bind-keys ("C-c C-9" . evil-leader-prefix-map))
+  (bind-keys :map evil-normal-state-map
+	     ("m" . er/expand-region)
+	     ("M-h" . paredit-backward)
+	     ("M-j" . paredit-forward-down)
+	     ("M-k" . paredit-backward-up)
+	     ("M-l" . paredit-forward)
+	     ("M-H" . paredit-backward-slurp-sexp)
+	     ("M-J" . paredit-forward-slurp-sexp)
+	     ("M-K" . paredit-forward-barf-sexp)
+	     ("M-L" . paredit-backward-barf-sexp))
+  (bind-keys :map evil-visual-state-map
+	     :prefix "SPC"
+	     :prefix-map evil-leader-prefix-map
+	     ("c" . comment-line)
+	     ("C" . comment-box))
+  (bind-keys :map evil-normal-state-map
+	     :prefix "SPC"
+	     :prefix-map evil-leader-prefix-map
+	     ("w" . save-buffer)
+	     ("SPC" . set-mark-command)
+	     ("C" . comment-box)
+	     ("c" . comment-line)
+	     ("x" . evil-exchange)))
+
+;; This overrides the default mark-in-region with a prettier-looking one,
+;; and provides a couple extra commands
+(use-package visual-regexp-steroids
+  :ensure t)
+
+(use-package visual-regexp
+  :ensure t
+  :config
+  (bind-keys :map evil-normal-state-map
+	     ("s" . vr/query-replace)
+	     ("?" . vr/replace)
+	     ("M-/" . vr/query-replace)))
+
+					;Emacs incremental search doesn't work with multiple cursors, but this fixes that
+(use-package phi-search
+  :bind (("C-s" . phi-search)
+	 ("C-r" . phi-search-backward)))
+
+;; Probably the first thing you'd miss is undo and redo, which requires an extra package
+;; to work like it does in kakoune (and almost every other editor).
+;; (use-package undo-tree
+;;   :config
+;;   (global-undo-tree-mode)
+;;   :ryo
+;;   ("u" undo-tree-undo)
+;;   ("U" unda-tree-redo)
+;;   ("SPC u" undo-tree-visualize)
+;;   :bind (:map undo-tree-visualizer-mode-map
+;;               ("h" . undo-tree-visualize-switch-branch-left)
+;;               ("j" . undo-tree-visualize-redo)
+;;               ("k" . undo-tree-visualize-undo)
+;;               ("l" . undo-tree-visualize-switch-branch-right)))
 
 (use-package expand-region
+  :ensure t)
+
+(use-package evil-paredit
   :ensure t)
 
 (use-package paredit
@@ -58,8 +139,9 @@
   :diminish paredit-mode
   :config
   (bind-keys :map paredit-mode-map
-		     ("{" . paredit-open-curly)
-		     ("}" . paredit-close-curly))
+	     ("{" . paredit-open-curly)
+	     ("}" . paredit-close-curly))
+  (evil-paredit-mode)
   :hook ((emacs-lisp-mode lisp-mode racket-mode scheme-mode) . paredit-mode))
 
 (use-package projectile
@@ -71,40 +153,24 @@
   (setq company-idle-delay 0.1)
   (setq company-tooltip-align-anotations t)
   :hook
-  ((racket-mode . company-mode)
+  ((emacs-lisp-mode . company-mode)
+   (racket-mode . company-mode)
    (racket-repl-mode . company-mode)))
+
+(use-package company-fuzzy
+  :init (global-company-fuzzy-mode 1))
 
 (use-package racket-xp-mode
   :hook racket-mode)
 
-(use-package evil
-  :config
-  (evil-mode)
-  (bind-keys ("C-c C-k" . evil-leader-prefix-map))
-  (bind-keys :map evil-normal-state-map
-	     ("C" . comment-box)
-	     ("m" . er/expand-region))
-  (bind-keys :map evil-normal-state-map
-	     :prefix "SPC"
-	     :prefix-map evil-leader-prefix-map
-	     ("x" . evil-exchange))
-  (bind-keys :map evil-visual-state-map
-	     :prefix "SPC"
-	     :prefix-map evil-leader-prefix-map
-	     ("x" . evil-exchange))
-  )
-
-
 (use-package treemacs
   :ensure t
   :defer t
-  :config (bind-keys ("º" . treemacs))
-  )
+  :config (bind-keys ("º" . treemacs)))
 
 (use-package treemacs-evil
   :ensure t
   :init (setq treemacs-no-png-images t))
-
 
 (use-package smex
   :init
@@ -118,7 +184,3 @@
   :when (display-graphic-p)
   :load-path "vendor/twilight-bright-theme"
   :config (load-theme 'twilight-bright t))
-
-(use-package phi-search
-  :bind (("C-s" . phi-search)
-         ("C-r" . phi-search-backward)))
